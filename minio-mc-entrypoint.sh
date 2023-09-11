d210abcd9fa46eecc7333f0c1047cb36d06a8173
Gabriel Diaz <gabrieldiaz31@gmail.com>
2023-09-11 17:29:23 -0300

    fix: Use .env variables to start up minio container

--- minio-mc-entrypoint.sh
+++ minio-mc-entrypoint.sh
@@ -8,13 +8,13 @@ sleep 5
 mc alias set minio http://minio:9000 admin password
 
 # Check if the bucket already exists
-if mc find minio/asset-packs ; then
-  echo "Bucket \"asset-packs\" already exists, no need to create it again"
+if mc find minio/$S3_BUCKET_NAME ; then
+  echo "Bucket \"$S3_BUCKET_NAME\" already exists, no need to create it again"
 else
   # Create the bucket and set the policy to public so anything can 
   # be downloaded or uploaded
-  mc mb minio/asset-packs
-  mc anonymous set public minio/asset-packs
+  mc mb minio/$S3_BUCKET_NAME
+  mc anonymous set public minio/$S3_BUCKET_NAME
 fi
 
 # Keep the service running so it can be accessed later with docker-compose exec
