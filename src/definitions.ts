66c6df7dbf22ec0480e06e8f43fead7ae1654f55
Gabriel Diaz <gabrieldiaz31@gmail.com>
2023-12-11 21:51:26 +0100

    fix: Left deprecated property for retrocompatibility

--- src/definitions.ts
+++ src/definitions.ts
@@ -65,6 +65,8 @@ export const ActionSchemas = {
   [ActionType.STOP_SOUND]: Schemas.Map({}),
   [ActionType.SET_VISIBILITY]: Schemas.Map({
     visible: Schemas.Boolean,
+    /** @deprecated use collider instead */
+    physicsCollider: Schemas.Optional(Schemas.Boolean),
     collider: Schemas.Optional(
       Schemas.EnumNumber(Colliders, Colliders.CL_POINTER),
     ),
