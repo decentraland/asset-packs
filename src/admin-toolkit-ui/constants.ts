8d98d52a031c80bed73096d7827f1c97d4d14e60
Gabriel Diaz <gabrieldiaz31@gmail.com>
2025-02-14 01:11:05 -0300

    fix: Use .zone content server

--- src/admin-toolkit-ui/constants.ts
+++ src/admin-toolkit-ui/constants.ts
@@ -1,5 +1,1 @@
-import { version } from '../../package.json'
-
-export const CONTENT_URL = version.includes('commit')
-  ? 'https://builder-items.decentraland.zone'
-  : 'https://builder-items.decentraland.org'
+export const CONTENT_URL = 'https://builder-items.decentraland.zone'
